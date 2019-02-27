e8cc520382690a7377f7799d8774e564f5a57a66
Ghostbullets <1067339066@qq.com>
2019-02-27 10:50:04 +0800

    1.增加recycleview2包下新的Adapter类

---
+++ base/src/main/java/com/hykj/base/adapter/recyclerview2/MultiRecycleViewAdapter.java
@@ -0,0 +1,42 @@
+package com.hykj.base.adapter.recyclerview2;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.SparseIntArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hykj.base.adapter.LayoutItem;
+
+import java.util.List;
+
+/**
+ * 多布局适配器
+ * @param <T>
+ */
+public abstract class MultiRecycleViewAdapter<T> extends BaseAdapter<T, BaseViewHolder> {
+    protected SparseIntArray mLayoutArray = new SparseIntArray();
+
+    public MultiRecycleViewAdapter(Context context, List<T> datas, LayoutItem... layoutItems) {
+        super(context, datas, 0);
+        for (int i = 0; i < layoutItems.length; i++) {
+            LayoutItem item = layoutItems[i];
+            mLayoutArray.put(item.getType(), item.getLayoutId());
+        }
+    }
+
+    @NonNull
+    @Override
+    public BaseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(mContext).inflate(mLayoutArray.get(viewType), parent, false);
+        return new BaseViewHolder(this,itemView, mListener);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return this.getItemViewType(this.getItem(position), position);
+    }
+
+    public abstract int getItemViewType(T t, int position);
+}
