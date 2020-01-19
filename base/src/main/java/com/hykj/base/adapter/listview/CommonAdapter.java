c63dda941b9b2a1bdc4ab86e120061830d6d3b14
Ghostbullets <1067339066@qq.com>
2020-01-19 17:14:08 +0800

    1.优化适配器用于androidx

--- base/src/main/java/com/hykj/base/adapter/listview/CommonAdapter.java
+++ base/src/main/java/com/hykj/base/adapter/listview/CommonAdapter.java
@@ -1,11 +1,12 @@
 package com.hykj.base.adapter.listview;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import androidx.annotation.NonNull;
 
 import java.util.ArrayList;
 import java.util.List;
