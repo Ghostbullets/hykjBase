7befe66088d63c310d1ca4dc7159dba759b1d4c5
Ghostbullets <1067339066@qq.com>
2020-03-06 10:51:44 +0800

    1.refresh文件夹下增加下拉刷新、上拉加载更多功能，使用方式见RecyclerViewDemoActivity 引用依赖方面有更改，如下所示: 在project下的build.gradle新增 buildscript {     ext.kotlin_version = '1.3.50'  //kotlin相关版本号  新增     dependencies {         classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version" //  新增     } } var = [ //新增       KotlinStdlib         : "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version",       KotlinReflect        : "org.jetbrains.kotlin:kotlin-reflect:$kotlin_version"     ]
    在module模块下的build.gradle新增
    apply plugin: 'com.android.application'
    apply plugin: 'kotlin-android'   //新增
    apply plugin: 'kotlin-android-extensions'   //新增
    
    dependencies {
      //kotlin 开发引用包、反射包   新增
        implementation var.KotlinStdlib
        implementation var.KotlinReflect
    }
    sync一下，即可

---
+++ app/src/main/java/com/hykj/hykjbase/RecyclerViewDemoActivity.kt
@@ -0,0 +1,75 @@
+package com.hykj.hykjbase
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.TextView
+import com.hykj.base.adapter.recyclerview2.BaseViewHolder
+import com.hykj.base.adapter.recyclerview2.SimpleRecycleViewAdapter
+import com.hykj.base.utils.text.Tip
+import com.hykj.base.view.refresh.CustomizeSwipeRefreshAndLoadMoreLayout
+import com.hykj.base.view.refresh.SimpleRefreshAndLoadMoreLayout
+import java.util.ArrayList
+
+class RecyclerViewDemoActivity : AppCompatActivity() {
+    private var contentAdapter: SimpleRecycleViewAdapter<String>? = null
+    private val contentList = ArrayList<String>()
+
+    private lateinit var refreshLoadMoreLayout: SimpleRefreshAndLoadMoreLayout
+    private val handler = Handler(Looper.getMainLooper())
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view_demo)
+        initView()
+    }
+
+    private fun initView() {
+        for (i in 0 until 30) {
+            contentList.add("这是第${i}个内容")
+        }
+        contentAdapter = createContentAdapter(contentList)
+
+        val rvContent = findViewById<RecyclerView>(R.id.rv_content)
+        rvContent.layoutManager = LinearLayoutManager(this)
+        val decoration = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        decoration.setDrawable(resources.getDrawable(R.drawable.divider_trans_v_10dp))
+        rvContent.addItemDecoration(decoration)
+        rvContent.adapter = contentAdapter
+
+        refreshLoadMoreLayout = findViewById(R.id.srl_refresh_load_more)
+        refreshLoadMoreLayout.isCanPullSurprised = true
+        //refreshLoadMoreLayout.isCanRefreshing=false
+        refreshLoadMoreLayout.isCanLoadMore = false
+        refreshLoadMoreLayout.setOnRefreshListener(object : CustomizeSwipeRefreshAndLoadMoreLayout.OnRefreshListener {
+            override fun onRefresh() {
+                handler.postDelayed({ refreshLoadMoreLayout.refreshFinish() }, 3000)
+
+            }
+        })
+        refreshLoadMoreLayout.setOnLoadMoreListener(object : CustomizeSwipeRefreshAndLoadMoreLayout.OnLoadMoreListener {
+            override fun onLoadMore() {
+                handler.postDelayed({ refreshLoadMoreLayout.loadMoreFinish() }, 3000)
+            }
+        })
+        refreshLoadMoreLayout.setOnPullSurprisedListener(object : CustomizeSwipeRefreshAndLoadMoreLayout.OnPullSurprisedListener {
+            override fun onPullSurprised() {
+                Tip.showShort("你打开了惊喜")
+                handler.postDelayed({ refreshLoadMoreLayout.resetScroll() }, 1000)
+            }
+        })
+    }
+
+    private fun createContentAdapter(contentList: ArrayList<String>): SimpleRecycleViewAdapter<String>? {
+        return object : SimpleRecycleViewAdapter<String>(this, contentList, R.layout.item_recycleview_text) {
+            override fun BindData(holder: BaseViewHolder, item: String, position: Int, payloads: MutableList<Any>) {
+                (holder.itemView as TextView).text = item
+            }
+        }
+    }
+}
