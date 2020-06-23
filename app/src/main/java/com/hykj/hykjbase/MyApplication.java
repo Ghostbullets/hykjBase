4809676f7ee0c7ee994601f80a0b88229f9acdd6
Ghostbullets <1067339066@qq.com>
2020-06-23 15:10:21 +0800

    1.  MultiDex.install(base);

--- app/src/main/java/com/hykj/hykjbase/MyApplication.java
+++ app/src/main/java/com/hykj/hykjbase/MyApplication.java
@@ -2,7 +2,8 @@ package com.hykj.hykjbase;
 
 import android.app.Application;
 import android.content.Context;
-import android.support.multidex.MultiDex;
+
+import androidx.multidex.MultiDex;
 
 import com.hykj.base.utils.ContextKeep;
 import com.hykj.base.utils.storage.FileUtil;
