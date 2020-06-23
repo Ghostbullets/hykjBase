ffd560e59a3fb7823ece711984897b74c743d073
Ghostbullets <1067339066@qq.com>
2020-06-23 15:09:31 +0800

    1.  MultiDex.install(base);

--- app/src/main/java/com/hykj/hykjbase/MyApplication.java
+++ app/src/main/java/com/hykj/hykjbase/MyApplication.java
@@ -1,11 +1,20 @@
 package com.hykj.hykjbase;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.hykj.base.utils.ContextKeep;
 import com.hykj.base.utils.storage.FileUtil;
 
 public class MyApplication extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(base);
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
